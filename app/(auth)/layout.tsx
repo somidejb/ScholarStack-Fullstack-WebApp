fa54e64d01487ad0de4dd18fbc33776c61817cf9
somidejb <jbsomide@gmail.com>
2024-05-24 02:24:12 -0600

    added the upload books route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex-center pt-10 min-h-screen w-full bg-[#D6DAEA] bg-cover bg-center">
       <div className="w-full flex md:flex-row flex-col gap-2 items-center md:items-start">
-        <div className="flex justify-end items-center flex-col w-full  md:w-1/2">
+        <div className="flex items-center flex-col w-full  md:w-1/2">
           <div className="text-[20px] md:text-[35px] leading-[24px] md:leading-[42px] font-bold flex flex-col text-end">
             <h1>
               Welcome to ScholarStack-<span className="h1-gradient"> where</span>
@@ -15,7 +15,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
             <h2 className ="h1-gradient">every book finds</h2>
             <h2 className="h1-gradient">a new home</h2>            
           </div>
-          <div className="flex h-[280px] lg:h-[500px] sm:h-full">
+          <div className="flex justify-center h-[280px] lg:h-[500px] sm:h-full">
             <Image src="/assets/images/saly.png" alt="hero" width={750} height={750} className="object-contain" />
           </div>
         </div>
