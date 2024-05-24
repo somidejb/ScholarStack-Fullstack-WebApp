9cfcfd3aefb61d72a85b5faa9c9fd88f6cea3f0b
somidejb <jbsomide@gmail.com>
2024-05-24 01:14:12 -0600

    Auth pages made responsive

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,18 +5,18 @@ import React from 'react';
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="justify-center items-center pt-10 min-h-screen w-full bg-[#D6DAEA] bg-cover bg-center">
-      <div className="flex md:flex-row flex-col gap-2">
-        <div className="items-center w-[50%]">
-          <div className="text-[35px] leading-[42px] font-bold font-inter flex flex-col justify-end text-end">
+    <div className="flex-center pt-10 min-h-screen w-full bg-[#D6DAEA] bg-cover bg-center">
+      <div className="w-full flex md:flex-row flex-col gap-2 items-center md:items-start">
+        <div className="flex justify-end items-center flex-col w-full  md:w-1/2">
+          <div className="text-[20px] md:text-[35px] leading-[24px] md:leading-[42px] font-bold flex flex-col text-end">
             <h1>
-              Welcome to ScholarStack-<span className="bg-gradient-to-45 from-[#D6DAEA] to-[#A6A9CE] bg-clip-text text-transparent text-right"> where</span>
+              Welcome to ScholarStack-<span className="h1-gradient"> where</span>
             </h1>
-            <h2 className ="bg-gradient-to-45 from-[#D6DAEA] to-[#A6A9CE] bg-clip-text text-transparent text-right">every book finds</h2>
-            <h2 className="bg-gradient-to-45 from-[#D6DAEA] to-[#A6A9CE] bg-clip-text text-transparent text-right">a new home</h2>            
+            <h2 className ="h1-gradient">every book finds</h2>
+            <h2 className="h1-gradient">a new home</h2>            
           </div>
-          <div className="flex justify-end">
-            <Image src="/assets/images/saly.png" priority={true} alt="hero" width={450} height={450} className="object-contain" />
+          <div className="flex h-[280px] lg:h-[500px] sm:h-full">
+            <Image src="/assets/images/saly.png" alt="hero" width={750} height={750} className="object-contain" />
           </div>
         </div>
         <div>
@@ -26,5 +26,6 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     </div>
   );
 };
+
 
 export default Layout;
