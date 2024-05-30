bdd5efad10bb599cdbe99c4ba085afc7c17672d3
Daniel-Akpan <95925992+TW-Warlord@users.noreply.github.com>
2024-05-30 08:52:54 -0600

    fixing logo rendering problem

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,6 +1,5 @@
+"use client";
 import React from "react";
-import Link from "next/link";
-import { headerLinks } from "@/constants";
 import {
   Sheet,
   SheetContent,
@@ -11,34 +10,40 @@ import {
 } from "@/components/ui/sheet";
 import NavItems from "./NavItems";
 import Image from "next/image";
+import { Separator } from "@radix-ui/react-separator";
 
 const MobileNav = () => {
   return (
     <nav className="md:hidden">
       <Sheet>
-        <SheetTrigger>
-            <Image
-              src="/assets/images/hamburger.png"
-              alt="Hamburgermenu"
-              width={32}
-              height={32}
-              />
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/images/hamburger.png"
+            alt="Hamburgermenu"
+            width={32}
+            height={32}
+            className="cursor-pointer"
+          />
         </SheetTrigger>
-        <SheetContent className = "flex flex-col gap-6 bg-white md:hidden">
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <div className="relative flex items-center mt-6">
+            <Separator className="flex-grow border border-gray-50" />
+            <div className="absolute left-1/2 transform -translate-x-1/2 flex items-center gap-2 bg-white px-2">
               <Image
-                src="/assets/images/scholarstacklogo.png"
+                src="/assets/images/mobilelogo.png"
                 alt="ScholarStackLogo"
                 width={24}
                 height={25}
               />
-              <h2 className="text-[24px] text-black font-semibold font-['PT_Serif']">
+              <h2 className="text-20 text-black font-semibold font-['PT_Serif']">
                 Scholar Stack
               </h2>
+            </div>
+          </div>
           <NavItems />
         </SheetContent>
       </Sheet>
     </nav>
-    
   );
 };
 
