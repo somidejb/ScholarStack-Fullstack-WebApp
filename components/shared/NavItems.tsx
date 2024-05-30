3a6310e321c9c63d3b5045f8fd37d2851a753935
Daniel-Akpan <95925992+TW-Warlord@users.noreply.github.com>
2024-05-30 14:33:30 -0600

    fix

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -2,13 +2,12 @@ import { headerLinks } from '@/constants';
 import Link from 'next/link';
 import React from 'react';
 
-
 const NavItems = ({ pathname }: { pathname: string }) => {
   return (
     <ul className="flex flex-col md:flex-row gap-8 tracking-widest md:gap-8 lg:gap-12">
       {headerLinks.map((link) => (
         <Link key={link.route} href={link.route}>
-          <li className={`flex items-center justify-between cursor-pointer ${pathname === link.route ? 'w-[235px] h-[43px] bg-blue-800 rounded-lg p-2 text-white' : 'text-black'}`}>
+          <li className={`flex items-center justify-between cursor-pointer ${pathname === link.route ? 'bg-blue-800 p-2 rounded-lg text-white' : 'text-black'}`}>
             <h2 className="text-lg font-medium md:text-white">
               {link.label}
             </h2>
@@ -21,4 +20,3 @@ const NavItems = ({ pathname }: { pathname: string }) => {
 };
 
 export default NavItems;
-
