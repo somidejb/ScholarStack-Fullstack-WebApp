b14904d43b39384acc7908c795dfebe39ba38117
osabuohiense <124754990+Akelvin51@users.noreply.github.com>
2024-06-03 12:54:02 -0600

    implements scrolling functionality

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -4,24 +4,20 @@ import { useState, useEffect } from 'react'
 import BookCard from './BookCard';
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { useSwipeable } from 'react-swipeable';
 
 type collectionProps = {
   collection_type: string
 }
-export const Collection = ({collection_type} : collectionProps) => {
+
+export const Collection = ({ collection_type }: collectionProps) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [cardsPerSlide, setCardsPerSlide] = useState(3);
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth >= 1280) {
-        setCardsPerSlide(5); // extra-large
-      } else if (window.innerWidth >= 1024) {
-        setCardsPerSlide(4); // large
-      } else {
-        setCardsPerSlide(3); // medium and small
-      }
+      const width = window.innerWidth;
+      setCardsPerSlide(Math.max(3, Math.floor((width - 1024) / 140) + 1));
     };
 
     window.addEventListener('resize', handleResize);
@@ -30,21 +26,26 @@ export const Collection = ({collection_type} : collectionProps) => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const totalSlides = Math.ceil(10 / cardsPerSlide);
+  const totalSlides = Math.ceil(books.length / cardsPerSlide);
 
   const handlePrevClick = () => {
     setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : totalSlides - 1);
   };
 
-  const handleNextClick = () => {
-    setCurrentSlide(currentSlide < totalSlides - 1 ? currentSlide + 1 : 0);
-  };
+  const handleNextClick = () => setCurrentSlide((currentSlide + 1) % totalSlides);
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => handleNextClick(),
+    onSwipedRight: () => handlePrevClick(),
+
+    trackMouse: true
+  });
 
   const renderDots = () => {
     return Array.from({ length: totalSlides }).map((_, index) => (
-      <span 
-        key={index} 
-        className={`h-2 w-2 md:h-3 md:w-3 rounded-full mx-1 cursor-pointer ${currentSlide === index ? 'bg-blue-500' : 'bg-gray-500'}`} 
+      <span
+        key={index}
+        className={`h-2 w-2 md:h-3 md:w-3 rounded-full mx-1 cursor-pointer ${currentSlide === index ? 'bg-blue-500' : 'bg-gray-500'}`}
         onClick={() => setCurrentSlide(index)}
       />
     ));
@@ -53,18 +54,18 @@ export const Collection = ({collection_type} : collectionProps) => {
   return (
     <section className="mt-[60px] lg:mt-[88px] items-center flex flex-col">
       <h2 className="text-center leading-[27px] md:leading-[36px] lg:leading-[73px] text-[22px] md:text-[30px] lg:text-[42px] tracking-widest font-normal">{collection_type}</h2>
-      <div className="w-full card-center flex items-center mt-[8px] md:mt-[38px] lg:mt-[50px] relative">
+      <div className="w-full card-center flex items-center mt-[8px] md:mt-[38px] lg:mt-[50px] relative" {...swipeHandlers}>
         <div onClick={handlePrevClick} className="absolute z-10 left-[12px] md:left-[30px] lg:left-[45px] cursor-pointer">
           <Image src="/assets/icons/left-icon.png" alt="Left Arrow" width={24} height={24} />
         </div>
         <div className="flex overflow-hidden w-full">
-          <div 
+          <div
             className="flex gap-[22px] md:gap-[36px] transition-transform duration-500"
-            style={{ transform: `translateX(-${currentSlide * 100 / totalSlides}%)` }}
+            style={{ transform: `translateX(-${currentSlide * (100 / totalSlides)}%)` }}
           >
             {books.map((book, index) => (
-              <div key={index} className={`flex  w-[${100 / cardsPerSlide}%]`}>
-                <BookCard 
+              <div key={index} className={`flex w-[${100 / cardsPerSlide}%]`}>
+                <BookCard
                   title={book.title}
                   imageUrl={book.image}
                   author={book.author}
@@ -89,5 +90,3 @@ export const Collection = ({collection_type} : collectionProps) => {
     </section>
   );
 };
-
-
