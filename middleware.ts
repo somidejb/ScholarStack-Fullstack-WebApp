d6a957292af763bba01e8d7dd9507caa9d3af77d
somidejb <62191635+somidejb@users.noreply.github.com>
2024-07-03 16:34:41 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
 
+const isProtectedRoute = createRouteMatcher([
+    '/books(.*)',
+    '/profile(.*)',
+    '/chats(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+})
 export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
