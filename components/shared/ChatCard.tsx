5adc45b24b77cf4c740391a5b6ac5d6d64718105
somidejb <jbsomide@gmail.com>
2024-08-06 05:53:32 -0600

    fixes approve bug

--- components/shared/ChatCard.tsx
+++ components/shared/ChatCard.tsx
@@ -41,30 +41,28 @@ const ChatCard = ({ chat, index, userId, handleSelectChat, currentUser, chats,  
     console.log("set chats type: ", typeof setChats);
 
     useEffect(() => {
-        if(currentUser){
-            pusherClient.subscribe(userId);
+        pusherClient.subscribe(userId);
 
-            const handleChatUpdate = (updatedChat: any) => {
-                setChats((allChats : any) => allChats.map((chat : IChat) => {
-                    if(chat._id === updatedChat.id){
-                        return { ...chat, messages: updatedChat.messages};
-                    }else{
-                        return chat;
-                    }
-                }))
-            }
-            const handleNewChat = (newChat: any) => {
-                setChats((allChats) => [...allChats, newChat]);
-            }
+        const handleChatUpdate = (updatedChat: any) => {
+            setChats((allChats : any) => allChats.map((chat : IChat) => {
+                if(chat._id === updatedChat.id){
+                    return { ...chat, messages: updatedChat.messages};
+                }else{
+                    return chat;
+                }
+            }))
+        }
+        const handleNewChat = (newChat: any) => {
+            setChats((allChats) => [...allChats, newChat]);
+        }
 
-            pusherClient.bind("update-chat", handleChatUpdate);
-            pusherClient.bind("new-chat", handleNewChat);
+        pusherClient.bind("update-chat", handleChatUpdate);
+        pusherClient.bind("new-chat", handleNewChat);
 
-            return () => {
-                pusherClient.unsubscribe(userId);
-                pusherClient.unbind("update-chat", handleChatUpdate);
-                pusherClient.unbind("new-chat", handleNewChat);
-            }
+        return () => {
+            pusherClient.unsubscribe(userId);
+            pusherClient.unbind("update-chat", handleChatUpdate);
+            pusherClient.unbind("new-chat", handleNewChat);
         }
     }, [currentUser]);
 
